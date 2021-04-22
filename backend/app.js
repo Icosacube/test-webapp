8085f03154d4d73c9b6bac667e985a58862b6f0b
Kia Sen Tan <kiat@student.unimelb.edu.au>
2021-04-22 15:55:06 +1000

    Made frontend

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,17 @@ const dotenv = require('dotenv').config()
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.get("/", function(req, res) {
     res.send(Quote.getQuote());
 });
 
-let port = process.env.PORT;
+let port = process.env.PORT_BACKEND;
 console.log(port);
 if(port == null || port == "") {
     port = 5000;
